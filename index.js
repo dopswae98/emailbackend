6334a7af2b516357476b25ca2ae4c95b7a3bf907
dopswae98 <ephraimmatarutse@gmail.com>
2023-06-07 11:24:48 +0200

    data

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ app.get("/", (req, res) => {
 app.listen(4000, () => {
   console.log("Example app listening on port $4000!");
 });
+
+app.get("/api/data", (req, res) => {
+  const data = [
+    { id: 1, name: "John", age: 30 },
+    { id: 2, name: "Jane", age: 25 },
+    { id: 3, name: "Bob", age: 40 },
+  ];
+  res.json(data);
+});
