c5c4e80081214f514dfc55e6ef1b70fab5f928fb
dopswae98 <ephraimmatarutse@gmail.com>
2026-06-13 21:54:29 -0700

    added secrets

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "kingpingx88@gmail.com",
-    pass: "bmspoqmidwltkygb",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
